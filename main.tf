0288bf33da0cddf8779d14fe3ff0067cd51338e0
Sowmya_DevOpsEngineer <126788385+sowmya-bm@users.noreply.github.com>
2024-04-12 17:54:46 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,58 @@
+resource "aws_s3_bucket" "s3" {
+  bucket = var.bucket_name
+}
+
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.s3.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.s3.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.s3.id
+  key    = "index.html"
+  source = "index.html"
+  acl = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.s3.id
+  key    = "error.html"
+  source = "error.html"
+  acl = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "image" {
+  bucket = aws_s3_bucket.s3.id
+  key    = "sowmya.jpeg"
+  source = "sowmya.jpeg"
+  acl = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.s3.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
